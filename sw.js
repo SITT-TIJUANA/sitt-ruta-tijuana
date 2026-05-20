00ea17ef9ad8ed236d18ac4a1d96588ac03a3bfa
SITT-TIJUANA <gjvazquez@tijuana.gob.mx>
2026-05-20 14:05:45 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-// SITT T101 Service Worker
-const CACHE = 'sitt-t101-v1';
+// Trabajador de servicios SITT T101
+const CACHE = 'sitt-t101-v2';
 
 self.addEventListener('install', function(e) {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then(function(cache) {
       return cache.addAll(['/sitt-ruta-tijuana/', '/sitt-ruta-tijuana/index.html']);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) { return key !== CACHE; })
+            .map(function(key) { return caches.delete(key); })
+      );
+    }).then(function() {
+      return self.clients.claim();
     })
   );
 });
